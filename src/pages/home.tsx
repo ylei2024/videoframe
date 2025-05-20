d1994699bec5041856aac66720a8f366c7a21167
ylei <2457738122@qq.com>
2025-05-20 19:29:21 +0800

    feat: 设置视频开始位置

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,11 +2,13 @@ import { TFunction } from "i18next"
 import { FFmpeg } from "@ffmpeg/ffmpeg"
 import { toBlobURL } from "@ffmpeg/util"
 import Button from "@mui/material/Button"
+import Slider from "@mui/material/Slider"
+import MuiInput from "@mui/material/Input"
 import { useTranslation } from "react-i18next"
 import { open } from "@tauri-apps/plugin-dialog"
 import { readFile } from "@tauri-apps/plugin-fs"
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded"
-import { useState, useRef, Dispatch, SetStateAction, MutableRefObject } from "react"
+import { useState, useRef, Dispatch, SetStateAction, MutableRefObject, useEffect } from "react"
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
@@ -98,6 +100,29 @@ const Control = (props: ControlProps) => {
   }
   const { t, ffmpeg, filepath } = props
   const [state, setState] = useState<State>(State.Run)
+  const [duration, setDuration] = useState<number>(100)
+  // position = h * 60 * 60 + m * 60 + s
+  const [position, setPosition] = useState<number>(0)
+  const [hour, setHour] = useState<number>(0)
+  const [minute, setMinute] = useState<number>(0)
+  const [second, setSecond] = useState<number>(0)
+  const changePosition = (value: number, type: string) => {
+    if (type == "hour") {
+      setHour(value)
+    } else if (type == "minute") {
+      setMinute(value)
+    } else if (type == "second") {
+      setSecond(value)
+    } 
+    if (type == "position") {
+      setPosition(value)
+      setHour(Math.floor(value / 3600))
+      setMinute(Math.floor((value % 3600) / 60))
+      setSecond(Math.floor(value % 60))
+    } else {
+      setPosition(hour * 60 * 60 + minute * 60 + second)
+    }
+  }
   const loadFFmpeg = async () => {
     if (!ffmpeg.current.loaded) {
       await ffmpeg.current.load({
@@ -105,12 +130,28 @@ const Control = (props: ControlProps) => {
         wasmURL: await toBlobURL("./ffmpeg-core.wasm", "application/wasm"),
         workerURL: await toBlobURL("./ffmpeg-core.worker.js", "text/javascript")
       })
+      // 创建存放数据的目录
+      await ffmpeg.current.createDir("data")
     }
   }
+  useEffect(() => {
+    const run = async () => {
+      await loadFFmpeg()
+      console.log(await ffmpeg.current.listDir("/data"))
+      ffmpeg.current.terminate()
+      setDuration(await getVideoDuration(filepath))
+      changePosition(0, "position")
+    }
+    run()
+  }, [filepath])
   const getVideoDuration = async (filepath: string) => {
+    if (!filepath) {
+      return 0
+    }
     await loadFFmpeg()
     const filename = filepath.split(/[/\\]/).pop() || "video.mp4"
-    await ffmpeg.current.writeFile(filename, await readFile(filepath))
+    const target_path = "/data/" + filename
+    await ffmpeg.current.writeFile(target_path, await readFile(filepath))
     await ffmpeg.current.ffprobe([
       "-v",
       "error",
@@ -118,14 +159,14 @@ const Control = (props: ControlProps) => {
       "format=duration",
       "-of",
       "default=noprint_wrappers=1:nokey=1",
-      filename,
+      target_path,
       "-o",
       "output.txt"
     ])
     const data = await ffmpeg.current.readFile("output.txt")
     const text = typeof data === "string" ? data : new TextDecoder("utf-8").decode(data)
     console.log("视频时长", text)
-    return parseFloat(text)
+    return Math.trunc(parseFloat(text))
   }
   const click = async () => {
     if (state == State.Running) {
@@ -134,12 +175,87 @@ const Control = (props: ControlProps) => {
       await delay(10000)
     } else if (state == State.Run) {
       setState(State.Running)
-      getVideoDuration(filepath)
     }
     setState(State.Run)
   }
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-2">
+      <div className="w-124 flex flex-row items-center gap-6">
+        <div className="w-24/124 text-gray-100 text-sm flex items-center justify-center gap-0.5">
+          <MuiInput
+            size="small"
+            value={String(hour).padStart(2, '0')}
+            inputMode="numeric"
+            onChange={(e) => {
+              changePosition(parseInt(e.target.value), "hour")
+            }}
+            sx={{
+              width: "1.5rem",
+              height: "1.5rem",
+              color: "white"
+            }}
+            inputProps={{
+              type: "text",
+              maxLength: 2,
+              inputMode: "numeric",
+              pattern: "[0-9]*",
+              style: { textAlign: "center" }
+            }}
+          />
+          <div className="h-full text-center">:</div>
+          <MuiInput
+            size="small"
+            value={String(minute).padStart(2, '0')}
+            inputMode="numeric"
+            onChange={(e) => {
+              changePosition(parseInt(e.target.value), "minute")
+            }}
+            sx={{
+              width: "1.5rem",
+              height: "1.5rem",
+              color: "white"
+            }}
+            inputProps={{
+              type: "text",
+              maxLength: 2,
+              inputMode: "numeric",
+              pattern: "[0-9]*",
+              style: { textAlign: "center" }
+            }}
+          />
+          <div className="h-full text-center">:</div>
+          <MuiInput
+            size="small"
+            value={String(second).padStart(2, '0')}
+            inputMode="numeric"
+            onChange={(e) => {
+              changePosition(parseInt(e.target.value), "second")
+            }}
+            sx={{
+              width: "1.5rem",
+              height: "1.5rem",
+              color: "white"
+            }}
+            inputProps={{
+              type: "text",
+              maxLength: 2,
+              inputMode: "numeric",
+              pattern: "[0-9]*",
+              style: { textAlign: "center" }
+            }}
+          />
+        </div>
+        <div className="w-104/124">
+          <Slider
+            size="small"
+            step={1}
+            value={position}
+            min={0}
+            max={duration}
+            onChange={(_, value) => changePosition(value, "position")}
+          />
+        </div>
+      </div>
       <div className="w-124 flex justify-end">
         <div className="cursor-pointer" onClick={click}>
           <Button
@@ -169,7 +285,7 @@ const Home = () => {
   const ffmpeg = useRef(new FFmpeg())
   const [filepath, setFilePath] = useState<string>("")
   return (
-    <div className="font-sans flex flex-col gap-3">
+    <div className="font-sans flex flex-col gap-7">
       <Input {...{ t, filepath, setFilePath }}></Input>
       <Control {...{ t, ffmpeg, filepath }}></Control>
     </div>
